14197daf2e44bf15b00c07515bd4e7321996b6f8
clintonjnelson <clintonjnelson@live.com>
2015-05-13 08:16:00 -0700

    update Users model & server for auth

--- models/User.js
+++ models/User.js
@@ -1,19 +1,50 @@
 'use strict';
 
 // bring in mongoose for db management
+var bcrypt = require('bcrypt-nodejs');
+var eat = require('eat');
 var mongoose = require('mongoose');
 
-// Setup schema via mongoose function
+// Setup schema via mongoose function, add basic for auth as sub-object
 var userSchema = mongoose.Schema({
+  eatoken: String,
+  eatimestamp: { type: Date, default: Date.now },
   username: String,
-  email: String,
-  passtoken: String,
-  created_at: Date
+  basic: {
+    email: { type: String, unique: true },
+    passtoken: String
+  }
 });
 
 // Validations
 userSchema.path('username').required(true);
-userSchema.path('username').index({unique: true});
+// userSchema.path('username').index({unique: true});   // bring back later
+
+// Mongoose methods to make userSchema do stuff via User
+userSchema.methods.genHash = function(password, callback) {  // may have to pass next or done
+  bcrypt.genSalt(8, function (err, salt) {
+    bcrypt.hash(password, salt, null, function(err, hash) {
+      if (err) throw err;
+      // STORE PASSWORD IN DB VIA CALLBACK(?)
+      // or this.basic.password = hash;
+    });
+  });
+};
+
+userSchema.methods.checkPassword = function(password, callback) {
+  bcrypt.compare(password, this.basic.password, function(err, res) {
+    if (err) throw err;
+    // if fails, res=false. if success, res=true
+    // DO SOMETHING WITH CALLBACK
+  });
+};
+
+userSchema.methods.genToken = function(secret, callback) {
+  eat.encode({ eatoken: this.eatoken, eatimestamp: this.eatimestamp }, secret, function(err, token) {
+    if (err) throw err;
+    // SEND TOKEN WITH CALLBACK(?)
+  });
+}
 
 // Export mongoose model with Name/schema
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,17 +2,27 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
 var app = express();                    // make app/server via express
-var usersRoutes = express.Router();     // make router
+
+// Express Routers
+var authRouter = express.Router()
+var usersRouter = express.Router();
 
 // Setup db & host to listen
+// SETUP THESE AS ENV VARIABLES!!!!!!
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/users_development');
 
-// Require in routes from users_routes & pass usersRouter to populate
-require('./routes/users_routes')(usersRoutes);
+// initialize passport strategy
+app.use(passport.initialize());
+
+// Populate Router by passing to routes file
+require('./routes/users_routes.js')(usersRouter);
+require('./routes/auth_routes.js')(authRouter, passport); // takes an initialized passport library!
 
 // Assign base route & Router of subroutes to app
-app.use('/api', usersRoutes);
+app.use('/api', usersRouter);
+app.use('/api', authRouter);
 
 // Start server on env port or default 3000
 app.listen(process.env.PORT || 3000, function() {
